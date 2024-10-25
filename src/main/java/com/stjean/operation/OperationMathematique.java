3cc45704d81b76cb1bdbea962ba53a058903c7ab
Mexes Signe <signemexes@gmail.com>
2024-10-25 08:58:03 +0100

    Add factorial method to OperationMathematique

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -1,6 +1,15 @@
 package com.stjean.operation;
 
 public class OperationMathematique {
+	
+	public boolean estPositif(int nombre) {
+	    return nombre > 0;
+	}
+	
+	public int factorial(int number) {
+	    if (number <= 1) return 1;
+	    return number * factorial(number - 1);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
