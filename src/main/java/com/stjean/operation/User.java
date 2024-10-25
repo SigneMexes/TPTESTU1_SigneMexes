deb4cab3750212eb4c06b4a85c722e1a0bd1b583
SigneMexes <160276056+SigneMexes@users.noreply.github.com>
2024-10-25 10:24:37 +0100

    Update User.java

--- src/main/java/com/stjean/operation/User.java
+++ src/main/java/com/stjean/operation/User.java
@@ -22,6 +22,63 @@ public class User {
         this.city = city;
         this.balancePersonnel = balancePersonnel;
     }
+
+        
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public double getBalancePersonnel() {
+		return balancePersonnel;
+	}
+
+	public void setBalancePersonnel(double balancePersonnel) {
+		this.balancePersonnel = balancePersonnel;
+	}
     
     public void add(User user) throws EmailInvalidException {
         if (!isValidEmail(user.email)) {
@@ -56,7 +113,5 @@ public class User {
         String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
         return email.matches(emailRegex);
     }
-
-
-    // Getters and setters for each field
+    
 }
