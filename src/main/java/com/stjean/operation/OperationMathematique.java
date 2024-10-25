9a98068a257e9d8ac0f3b7f77506940b4cfef067
Mexes Signe <signemexes@gmail.com>
2024-10-25 09:05:03 +0100

    Add sort method to sort an array in descending order

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -1,5 +1,8 @@
 package com.stjean.operation;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class OperationMathematique {
 	
 	public boolean estPositif(int nombre) {
@@ -10,6 +13,14 @@ public class OperationMathematique {
 	    if (number <= 1) return 1;
 	    return number * factorial(number - 1);
 	}
+	
+
+
+	public Integer[] sort(Integer[] lists) {
+	    Arrays.sort(lists, Collections.reverseOrder());
+	    return lists;
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
